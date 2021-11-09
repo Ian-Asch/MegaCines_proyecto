2dd02994978bb4de295404b4746835ff45bedcba
Ian-Asch <iascht@ucenfotec.ac.cr>
2021-11-08 21:44:23 -0600

    javascript verificar

--- public/js/verificaciones_controlador.js
+++ public/js/verificaciones_controlador.js
@@ -2,13 +2,21 @@
 
 const lista_inputs_requeridos =  document.querySelector("[required]");
 
-let 
+const verificar_espacios = () => {
+    let verfificado = true;
+    
+    lista_inputs_requeridos.forEach((element) => {
+        if (element.value) {
+            element.classList.remove("error");
+        } else {
+            element.classList.add("error");
+            verfificado = false;
+        }
+    });
+};
 
-lista_inputs_requeridos.forEach((element) => {
-    if (element.value) {
-        element.classList.remove("error");
-    } else {
-        element.classList.add("error");
-        verfificado = false;
-    }
-});
+const verificar_correo = (input) => {
+    let expresion_correo = /^[a-z]+@[a-z]+\.(com|net|org|ac|cr)$/i;
+
+    
+};
