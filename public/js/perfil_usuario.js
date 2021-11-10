466a41db301ff01e58ed70366b92839dba02d15c
Ian-Asch <iascht@ucenfotec.ac.cr>
2021-11-10 16:06:49 -0600

    Update perfil_usuario.js

--- public/js/perfil_usuario.js
+++ public/js/perfil_usuario.js
@@ -5,7 +5,7 @@ let usuario = {
     primer_apellido: "Oviedo",
     segundo_apellido: "Murillo",
     correo: "juan@gmail.com",
-    cedula: "1-4235-8753",
+    cedula: "112354538",
     fecha_nacimiento: "5-2-1990",
     edad: 32
 }
