92d937de5c470d5643ab063a6cce11f0273a6227
Yogyx <aoviedoc@ucenfotec.ac.cr>
2021-11-11 17:11:27 -0600

    contrasenna

---
+++ public/js/cambio_de_contrasena.js
@@ -0,0 +1,11 @@
+const btn_contrasena = document.querySelector("#btn-ingresar")
+btn_contrasena.addEventListener("click",Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Your work has been saved',
+    showConfirmButton: false,
+    timer: 1500
+  }) )
+
+
+
