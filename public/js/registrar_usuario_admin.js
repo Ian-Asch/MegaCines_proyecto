207e3b84fa0bfef6f4aa99170bdb286ed2e37253
EnivinS <91513402+EnivinS@users.noreply.github.com>
2021-11-20 23:44:52 -0600

    updates, js

---
+++ public/js/registrar_usuario_admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const input_nombre_registro = document.querySelector("#nombre");
+const input_segundo_nombre_registro = document.querySelector("#segundo_nombre");
+const input_primer_apellido_registro = document.querySelector("#primer_apellido");
+const input_segundo_apellido_registro = document.querySelector("#segundo_apellido");
+const input_identificacion_registro = document.querySelector("#cedula");
+const input_nacimiento_registro_admin = document.querySelector("nacimiento");
+const input_edad_registro_admin = document.querySelector("#edad");
+const input_correo_registro_admin = document.querySelector("#correo");
