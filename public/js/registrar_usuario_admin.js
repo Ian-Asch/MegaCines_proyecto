3b20f1f5121872c60a1eebe7398d634b8124751e
EnivinS <91513402+EnivinS@users.noreply.github.com>
2021-11-25 22:03:49 -0600

    x

--- public/js/registrar_usuario_admin.js
+++ public/js/registrar_usuario_admin.js
@@ -8,3 +8,25 @@ const input_identificacion_registro = document.querySelector("#cedula");
 const input_nacimiento_registro_admin = document.querySelector("nacimiento");
 const input_edad_registro_admin = document.querySelector("#edad");
 const input_correo_registro_admin = document.querySelector("#correo");
+const botonGuardar = document.querySelector("#boton-guardar");
+
+const obtenerDatos = () => {
+    let nombre = input_nombre_registro.value;
+    let segundoNombre = input_segundo_nombre_registro.value;
+    let apellido = input_primer_apellido_registro.value;
+    let segundoApellido = input_segundo_apellido_registro.value;
+    let identificacion = input_identificacion_registro.value;
+    let nacimiento = input_nacimiento_registro_admin.value;
+    let edad = input_edad_registro_admin.value;
+    let correo = input_correo_registro_admin.value;
+}
+
+const validar = () => {
+    let error = false;
+    if (input_nombre_registro.value == "") {
+        error = true;
+        input_nombre_registro.classList.add("error-input");
+    }
+}
+
+botonGuardar.addEventListener('click', validar)
