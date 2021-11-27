e454044a2843ba27dc8301e99a185cb4aa2db26c
EnivinS <91513402+EnivinS@users.noreply.github.com>
2021-11-26 23:00:50 -0600

    x

--- public/js/registrar_usuario_admin.js
+++ public/js/registrar_usuario_admin.js
@@ -19,14 +19,79 @@ const obtenerDatos = () => {
     let nacimiento = input_nacimiento_registro_admin.value;
     let edad = input_edad_registro_admin.value;
     let correo = input_correo_registro_admin.value;
+    Swal.fire({
+        'icon': 'éxito',
+        'title': 'información valida',
+        'text': 'usuario guardado exitosamente'
+    });
 }
 
+
+
 const validar = () => {
+    let expresion_correo = /^[a-z]+@[a-z]+\.(com|net|org|ac|cr)$/i;
+    // let expresion_contraseña = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s\d]).+$/;
+    let expresion_solo_letras = /^[a-z\s]+$/i;
+    let expresion_cedula = /^\d{9,12}$/;
+
     let error = false;
+
     if (input_nombre_registro.value == "") {
         error = true;
         input_nombre_registro.classList.add("error-input");
+    } else {
+        input_nombre_registro.classList.remove("error-input");
     }
+
+    if (input_primer_apellido_registro.value == "") {
+        error = true;
+        input_primer_apellido_registro.classList.add("error-input");
+    } else {
+        input_primer_apellido_registro.classList.remove("error-input");
+    }
+
+    if (input_segundo_apellido_registro.value == "") {
+        error = true;
+        input_segundo_apellido_registro.classList.add("error-input");
+    } else {
+        input_segundo_apellido_registro.classList.remove("error-input");
+    }
+
+    if (input_identificacion_registro.value == "") {
+        error = true;
+        input_identificacion_registro.classList.add("error-input");
+    } else {
+        input_identificacion_registro.classList.remove("error-input");
+    }
+
+    if (input_nacimiento_registro_admin.value == "") {
+        error = true;
+        input_nacimiento_registro_admin.classList.add("error-input");
+    } else {
+        input_nacimiento_registro_admin.classList.remove("error-input");
+    }
+
+    if (input_correo_registro_admin.value == "") {
+        error = true;
+        input_correo_registro_admin.classList.add("error-input");
+    } else {
+        input_correo_registro_admin.classList.remove("error-input");
+    }
+
+    if (error == false) {
+        obtenerDatos()
+    } else {
+        Swal.fire({
+            icon: 'warning',
+            title: 'información incompleta',
+            text: 'por favor llene todos los espacios'
+        });
+
+
+    }
+
+
+
 }
 
 botonGuardar.addEventListener('click', validar)
