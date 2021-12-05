1b8d008a6ee457940686a60a84bdc878cf9b3003
EnivinS <91513402+EnivinS@users.noreply.github.com>
2021-12-05 12:41:08 -0600

    x

---
+++ public/js/listar_usuarios.js
@@ -0,0 +1,7 @@
+const lista_cines = document.querySelectorAll(".div-cine");
+
+lista_cines.forEach((cine) => {
+    cine.addEventListener("click", () => {
+        window.location.href = "perfil_usuario_admin.html";
+    });
+});
