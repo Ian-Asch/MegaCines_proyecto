d38eacee9659d2551478e72c9ff8e9b2c799e5f4
Ian-Asch <iascht@ucenfotec.ac.cr>
2021-12-07 00:18:45 -0600

    backend peliculas

--- api/models/peliculas.model.js
+++ api/models/peliculas.model.js
@@ -7,10 +7,16 @@ const schema_pelicula = new mongoose.Schema({
     descripcion: {type: String, required: false, unique: false},
     ano: {type: Number, required: true, unique: false},
     duracion: {type: String, required: true, unique: false},
-    // categorias: {type: [String], required: false, unique: false},
+    categorias: {type: [String], required: false, unique: false}, //Este es una lista de categorias y la lista debe ser de solo strings
     idioma: {type: String, required: true, unique: false},
-    subtitulos: {type: Boolean, required: true, unique: false},
-    idioma_subtitulos: {type: String, required: false, unique: false}
+    subtitulos: {type: Boolean, required: true, unique: false},//Este atributo es bolean entonces su valor solo puede ser true o false.
+    idioma_subtitulos: {type: String, required: false, unique: false}/*,
+    tandas: [{ //Esta parte no se si se va a usar. Por ahora no tocar
+        fecha: , //La fecha y la hora podrian ser un mismo atributo de tipo Date
+        hora: ,
+        sala: {type: String, required: , unique: },
+        cine: {type: String, required: , unique: }
+    }]*/
 });
 
 module.exports = mongoose.model('Pelicula',schema_pelicula,'Peliculas')
