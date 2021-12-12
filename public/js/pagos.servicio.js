9f53227a1fceceea82b69f4d738f07e9e81c6780
EnivinS <91513402+EnivinS@users.noreply.github.com>
2021-12-12 12:17:31 -0600

    x

---
+++ public/js/pagos.servicio.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const registrar_pelicula = async(nombre_tarjeta, numero, tipo, expiracion, cvv, nombre_dueno, direccion, provincia, postal) => {
+    let response = await axios({
+        method: 'post',
+        url: 'http://localhost:3000/api/registrar-pago',
+        responseType: 'json',
+        data: {
+            nombre_tarjeta: nombre_tarjeta,
+            numero: numero,
+            tipo: tipo,
+            expiracion: expiracion,
+            cvv: cvv,
+            nombre_dueno: nombre_dueno,
+            direccion: direccion,
+            provincia: provincia,
+            postal: postal
+        }
+    });
+
+    if (response.data.err) {
+        Swal.fire({
+            title: 'Error. No se pudo registrar el metodo de pago',
+            text: "Hubo un error al registrar el metodo de pago",
+            icon: 'error'
+        });
+    } else {
+        Swal.fire({
+            title: "El metodo de pago se registró exitósamente",
+            icon: 'success'
+        });
+        console.log(response.data.err);
+    }
+};
+
+const listar_peliculas = async() => {
+    let lista_peliculas = [];
+
+    await axios({
+        method: 'get',
+        url: 'http://localhost:3000/api//listar-pago',
+        responseType: 'json'
+    }).then((response) => {
+        lista_peliculas = response.data.lista_peliculas;
+    });
+    return lista_peliculas;
+};
