d48ae1ad9a0b5124cf02cf1162b550fd34db5206
Ian-Asch <iascht@ucenfotec.ac.cr>
2021-12-17 13:35:56 -0600

    .

--- public/js/listar_tandas.js
+++ public/js/listar_tandas.js
@@ -25,14 +25,20 @@ const mostarar_tandas_todo = async() => {
         let td_nombre_pelicula = document.createElement("td");
         td_nombre_pelicula.innerText = tanda.pelicula;
 
-        let td_fecha = document.createElement("td");
-        td_fecha.innerText = tanda.fecha
+        let td_fecha_inicio = document.createElement("td");
+        td_fecha_inicio.innerText = tanda.inicio
+
+        let td_fecha_finalizacion = document.createElement("td");
+        td_fecha_finalizacion.innerText = tanda.finalizacion;
+
+        let td_hora = document.createElement("td");
+        td_hora.innerText = tanda.hora;
 
         let td_sala = document.createElement("td");
         td_sala.innerText = tanda.sala
 
-        let td_cine = document.createElement("td");
-        td_cine.innerText = tanda.cine
+        let td_precio = document.createElement("td");
+        td_precio.innerText = tanda.precio
 
         let td_btn_editar = document.createElement("td");
         let boton_editar = document.createElement("button");
@@ -48,12 +54,14 @@ const mostarar_tandas_todo = async() => {
 
         td_btn_eliminar.appendChild(boton_eliminar)
 
-        table_row.appendChild(td_nombre_pelicula)
-        table_row.appendChild(td_fecha)
-        table_row.appendChild(td_sala)
-        table_row.appendChild(td_cine)
-        table_row.appendChild(td_btn_editar)
-        table_row.appendChild(td_btn_eliminar)
+        table_row.appendChild(td_nombre_pelicula);
+        table_row.appendChild(td_fecha_inicio);
+        table_row.appendChild(td_fecha_finalizacion);
+        table_row.appendChild(td_hora);
+        table_row.appendChild(td_sala);
+        table_row.appendChild(td_precio);
+        table_row.appendChild(td_btn_editar);
+        table_row.appendChild(td_btn_eliminar);
 
         tabla.appendChild(table_row);
 
@@ -71,12 +79,8 @@ const mostarar_tandas_todo = async() => {
             })
 
             mostarar_tandas_todo()
-        })
-
-        // new Date(tanda.fecha).getFullYear();
-        // new Date(tanda.fecha).getMonth();
-        // new Date(tanda.fecha).getDay();
+        });
     });
 }
 
-mostarar_tandas_todo()
+mostarar_tandas_todo();
